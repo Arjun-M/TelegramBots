ca08510e11b9015bf9ad6444f0c622e1a7a95c57
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-04-09 22:20:18 +0530

    Create core.py

---
+++ MultipleBots/bot/core.py
@@ -0,0 +1,6 @@
+from pyTelegramClient import Client , Telegram
+
+client1 = Client( token = "TOKEN 1" )
+client2 = Client( token = "TOKEN 2" )
+Bot1 = Telegram( client = client )
+Bot1 = Telegram( client = client )
