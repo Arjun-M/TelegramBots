1161f423167fe37ed2ee3077e1b8a03fad5c872b
Arjun <86394506+Arjun-M@users.noreply.github.com>
2022-04-09 21:21:42 +0530

    Create __main__.py

---
+++ NoPMsBot/bot/__main__.py
@@ -0,0 +1,24 @@
+import glob
+from .core import client 
+from pathlib import Path
+import sys , importlib , logging
+
+def load_plugins(plugin_name):
+    path = Path(f"bot/plugins/{plugin_name}.py")
+    name = "bot.plugins.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["botot.plugins." + plugin_name] = load
+    print("pyTelegramClient has Imported " + plugin_name)
+
+path = "bot/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+client.polling()
